ac0891d8b21ebd6dddace4001d42b35acbb84088
Jodelstefan <janeckbersch@outlook.com>
2023-10-12 10:22:52 +0200

    pycalculator Changed

--- picalculator.py
+++ picalculator.py
@@ -1,18 +1,11 @@
-from math import sqrt, pi
-
-def picalc(n_corners, s_length):#n_corners n Anzahl Ecken/ s_length Seitenlänge
-    for index in range(1, 31):
-        pi_approach = 0.5*n_corners*s_length
-        print(round(pi_approach, 11))
-        s_length = sqrt(2-sqrt(4-s_length*s_length))
-        n_corners = 2*n_corners
-    print("Ungeeignete Iteration!")
-    print("pi = ", round(pi, 11))
+def picalc(f_diameter, f_circlelength, i_round=11):#f_diameter Durchmesser/f_circlelength Umfang
+    return round((f_circlelength/f_diameter), i_round)
 
 def main():
-    n_corners = float(input("Geben Sie die Anzahl der Ecken ein: "))
-    s_length = float(input("Geben Sie die Seitenlänge ein: "))
-    picalc(n_corners, s_length)
+    f_diameter = float(input("Bitte geben Sie den Durchmesser ein: "))
+    f_circlelength = float(input("Bitte geben Sie den Umfang ein: "))
 
+    print(picalc(f_diameter, f_circlelength))
+    
 if __name__ == "__main__":
     main()

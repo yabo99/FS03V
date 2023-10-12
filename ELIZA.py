0a7cc656a98ec891f3c0a77c9a1e5f6d6a965280
Jodelstefan <janeckbersch@outlook.com>
2023-10-12 10:33:17 +0200

    import picalculator

--- ELIZA.py
+++ ELIZA.py
@@ -3,7 +3,7 @@ import wetter
 import random
 import nwurzel
 import wurzel
-
+import picalculator
 
 word = []
 numbers = []
@@ -64,7 +64,8 @@ def search(word):
                     if x.isdigit():
                         x = float(x)
                         return wurzelziehen(x)
-
+            if i == "pi":
+                return picalculator.picalc()
         
             return answer[i]
             

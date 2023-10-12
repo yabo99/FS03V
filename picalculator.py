0b96194d8d929e9f69f3438a3f3e6667e80cc5c0
Jodelstefan <janeckbersch@outlook.com>
2023-10-12 10:09:11 +0200

    picalculator changed

--- picalculator.py
+++ picalculator.py
@@ -6,3 +6,13 @@ def picalc(n_corners, s_length):#n_corners n Anzahl Ecken/ s_length Seitenlänge
         print(round(pi_approach, 11))
         s_length = sqrt(2-sqrt(4-s_length*s_length))
         n_corners = 2*n_corners
+    print("Ungeeignete Iteration!")
+    print("pi = ", round(pi, 11))
+
+def main():
+    n_corners = float(input("Geben Sie die Anzahl der Ecken ein: "))
+    s_length = float(input("Geben Sie die Seitenlänge ein: "))
+    picalc(n_corners, s_length)
+
+if __name__ == "__main__":
+    main()
